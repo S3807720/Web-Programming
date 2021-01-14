a0422b1335298e7261ec317a170ea2fe87b7a374
S3807720 <51018322+S3807720@users.noreply.github.com>
2021-01-15 03:05:31 +1100

    Add files via upload
    fixed up some alignment in index. and begun work on the action description.
    design mostly done, as is responsiveness.

--- scripts.js
+++ scripts.js
@@ -6,7 +6,32 @@ function myFunction() {
   } else {
     x.style.display = "block";
   }
-}function toggleWireframe() {
+}
+// https://medium.com/dev-genius/how-to-make-javascript-sleep-or-wait-d95d33c99909
+const sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+
+// using the above function i've made a toggle for the
+// letters and postcards
+async function togglePage(idName) {
+  var y, b;
+  for (var i = 1; 6 > i; i++){
+    let x = 'page' + i;
+    if (x == idName) {
+      y = document.getElementById(x);
+    }
+    b = document.getElementById(x);
+    b.style.display = "none";
+  }
+  //slowly fade in, without using jquery
+  y.style.display = "inline-block";
+  y.style.opacity = "45%";
+  await sleepNow(50);
+  y.style.opacity = "75%";
+  await sleepNow(25);
+  y.style.opacity = "100%";
+}
+
+function toggleWireframe() {
 
   // get references to styleshets with preset id attributes
   let wfs = document.getElementById("wireframecss");
