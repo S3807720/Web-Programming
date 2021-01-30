321960512f23693c2e0464104e0058b0158d59e5
S3807720 <51018322+S3807720@users.noreply.github.com>
2021-01-30 23:20:47 +1100

    Add files via upload
    copied a2 over. added basic php functionality for header and nav reuse

--- a3/index.php
+++ a3/index.php
@@ -1,41 +1,141 @@
-<!DOCTYPE html>
-<html lang='en'>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Assignment 3</title>
+<?php require_once("tools.php"); ?>
+<?php top_module("ANZAC Douglas Raymond Baker"); ?>
+  <main class="container">
+    <img src="https://discoveringanzacs.naa.gov.au/media/mawson/108032.png"
+    class ="portrait" alt="portrait of douglas raymond baker">
+    <section class="home">
+      <p>
+        Hello and welcome,
+        This year is the centenary of the birth of the ANZAC legend. As such, many people, particularly young people,
+        are looking for ways to connect with people of that period and inparticular, those who created the ANZAC legend.
+      </p>
+      <p>
+        This site presents the letters of Douglas Raymond Baker, who came from Gympie, Queensland, Australia.
+      </p>
 
-    <!-- Keep wireframe.css for debugging, add your css to style.css -->
-    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-    <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
-    <script src='../wireframe.js'></script>
-  </head>
+      <p>
+        He enlisted in August 1914 and during the years that followed, he wrote letters and post cards to his family at home.
+        In them, he describes much of the goings-on of the life of an ANZAC, his feeling and opinions, and experiences while visiting
+        his relatives in England during leave.
+      </p>
+      <p>
+        They start from the beginning of basic training in Brisbane in August 1914 and end in May 1918.
+        They are offered here so that others may get an understanding of life as an ANZAC and get a sense of what life on the
+        battlefield was like.
+      </p>
+      <p>
+        First read the introduction below to set the scene. Then from the menu on the left, to start reading the letters,
+        click on Letter and Post Cards in the menu on the left.
+      </p>
+      <p>
+        <i>(Photograph courtesy of : https://discoveringanzacs.naa.gov.au/browse/person/63381 [Image number: 3])</i>
+      </p>
 
-  <body>
+    </section>
 
-    <header>
-	  <div> //for some reason there was no index.php for a3 </div>
-      <div>Put company logo and name here</div>
-    </header>
+    <section class="home">
+      <h4> Introduction </h4>
+      <h4>By Douglas Richard Baker
+        (son of Douglas Raymond Baker)</h4>
+        <p>
+          These are copies of letters written by my father Douglas Raymond Baker during the First World War (1914-1918)
+          to his family in Gympie. I have three thick exercise books in which the letters were copied in hand writing
+          by my Aunt Alice, his sister. I understand that this was done so his letters could be sent on to other members
+          of the family. I don’t know if the originals are still in existence, probably not.
+        </p>
+        <p>
+          In the early letters Alice has not included the name of the person they were written to but later she writes at
+          the beginning, “Letter to Dot”,  “Letter to Mother”, etc. and later still copies the original, “Dear Al”, “Dear Mag”,
+          etc..  Likewise the endings are often not laid out fully as we would do but Al may have done this or Dad may have
+          been saving space on the page.
+        </p>
+        <p>
+          Some letters are slightly out of date order in the books but as most of these are around
+          the time his father died I have put them in the correct order as this is crucial to understanding
+          the sense of the contents.  A lot will be missing – many, many ships were sunk but thanks to Aunt
+          Alice we have these copies of the ones that did get through.
+        </p>
+        <p>
+          Where Dad used brackets in a letter I have used [   ]  style and where I have made any comment or
+          explanation I have used (   ) and Italics.
+        </p>
+        <p>
+          Where he uses the word “gay” it is used in the true sense, bright, happy, carefree, etc., This is the
+          original meaning of the word before it became associated with the homosexual community.
+        </p>
+        <p>
+          The amounts of money are, of course, in Pounds, shillings and pence but as these are out of date and we
+          have no pounds sign in the computer I have written them in, sometimes showing the decimal equivalents.
+          One Pound equalled $2,  one shilling equalled 10 cents and one penny equalled a little under one cent.
+          After a while I stopped putting the equivalents in as they had cno significance unless you knew the relative
+          cost of things then and now. A shilling – equivalent to ten cents might actually alent to ten cents might
+          actually buy  buy ten or twenty dollars worth of goods now.
+        </p>
+        <p>
+          Some abbreviations and terms:
+        </p>
+        <ul>
+          <li>Coy = Company(part of a Battalion)</li>
+          <li>  Batt. = Battalion.</li>
+          <li>  Col. = Colonel.</li>
+          <li>  Sergt. = Sergeant.</li>
+          <li>  Capt. = Captain</li>
+          <li>  Lieut. = Lieutenant (pronounced “Lef-tenant”)</li>
+          <li>Q.M. = Quarter Master.</li>
+          <li>  Comp. = also  Company.</li>
+          <li>  PC = Post Card.(not Personal Computer!)</li>
+          <li>  CB = Confined to Barracks (leave stopped as a punishment)</li>
+          <li>  big hits = top ranked officers.</li>
+          <li>    brass hats = top ranked officers.</li>
+          <li>  dry canteen = bar and/or cafeteria, etc. where  no alcohol is served.</li>
+          <li>    wet canteen = bar and/or cafeteria, etc. where alcohol  is available.</li>
+          <li>  sapping = digging a tunnel or deep trench to approach or undermine an enemy position.</li>
+          <li>  L. Horse = Light Horse.</li>
+          <li>  good nick or great nick = good health</li>
+          <li>  bonsorful = wonderful, marvellous. (in 2009speak,  “awesome”, “cool”.)</li>
+          <li>  M. G. S. = Machine  Gun  Section.</li>
+          <li>  Territorials = soldiers of the English  Volunteer Reserve.</li>
+          <li>    Terriers = Territorials</li>
+          <li>  Bomb = unless dropped from an aeroplane these would have been Mills Bombs – an early version of the hand grenade.</li>
+          <li>  Blighty = England.</li>
+          <li>  Duke = Dad’s/family’s dog.</li>
+          <li>O.R. = Orderly Room.</li>
+        </ul>
+        <h4>
+          Additional Comments by Ian Stuart Baker
+          (son of Douglas Richard Baker)
+        </h4>
 
-    <nav>
-      <div>Put website navigation links here</div>
-    </nav>
-
-    <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
-      </article>
+        <p>
+          As the grandson of the Douglas Raymond, I'd like to share some thoughts with modern readers.
+          Apart from the language issues that my Dad highlights, it would also help to recognise that
+          in Grandfathers day, the letter was the only means of international communications for average
+          people (telegrams were expensive and used rarely and international telephone calls, rarer still).
+          That's why these letters are so important and why their content gives us window into their lives.
+        </p>
+        <p>
+          From discussions with my Dad, it has came to light that the content of letters to his direct family
+          deliberately leave out much of the dreadful suffering and drudgery experienced by the diggers. This
+          intentional self-editing was intended to allay the fears and concerns of those at home, inparticular,
+          his mother and sisters.
+        </p>
+        <p>
+          Finally, let me express a debt of thanks to Great Aunt Alice for diligently transcribing the original
+          letters for without this effort, we would not have the material we do today. Also, my gratitude to my
+          father for his work transforming the hand-written script into a typed paper record and then into electronic
+          format, making my part in this infinitely easier.
+        </p>
+      </section>
     </main>
 
     <footer>
       <div>&copy;<script>
-        document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
-      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
-      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
-    </footer>
+      document.write(new Date().getFullYear());
+      </script> Luke Smith, S3807720. <a href="https://github.com/S3807720/wp" target="blank"> Github repo</a>. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      <div>Disclaimer: This website is not a real website and is being developed as part of a School of
+        Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+        <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+      </footer>
 
-  </body>
-</html>
+    </body>
+    </html>
--- a3/tools.php
+++ a3/tools.php
@@ -1,6 +1,48 @@
 <?php
-  session_start();
+session_start();
+function top_module($pageTitle) {
+  $html = <<<"OUTPUT"
+  <!DOCTYPE html>
+  <html lang='en'>
+  <head>
+  <meta charset="UTF-8">
+  <meta name="description" content="This site records the letters home during WW1 from ANZAC Douglas
+  Raymond Baker, from September 1914 after he joined up in Gympie, Queensland, Australia to May 1918.">
+  <meta name="keywords" content="Letters & Post Cards - ANZAC Douglas Raymond Baker">
+  <meta name="author" content="Ian Stuart Baker">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-// Put your PHP functions and modules here
+  <title>$pageTitle</title>
+  <!-- Keep wireframe.css for debugging, add your css to style.css -->
+  <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+  <link id='stylecss' type="text/css" rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
+  <script src='../wireframe.js'></script>
+  <script src='../scripts.js'></script>
+  </head>
+
+  <body>
+
+  <header class = "banner">
+  <h2>ANZAC Douglas Raymond Baker</h2> <h3> Letters Home</h3>
+  </header>
+
+  <nav>
+  <!-- https://www.w3schools.com/howto/howto_js_mobile_navbar.asp -->
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+  <i class="fa fa-bars"></i>
+  </a>
+  <div id="navLinks">
+  <a href="index.php" class="active">Home</a>
+  <a href="postletters.html">Letters & Post Cards</a>
+  <a href="action-description.html">Action Description</a>
+  <a href="contact.html">Contact</a>
+  </div>
+
+  </nav>
+  OUTPUT;
+  echo $html;
+}
 
 ?>
