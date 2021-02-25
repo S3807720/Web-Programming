5d6e4dd14b433d86690c0023bbde9c758b2fc178
S3807720 <51018322+S3807720@users.noreply.github.com>
2021-02-25 19:05:29 +1100

    Delete testpage.php
    junk file

--- a3/testpage.php
+++
@@ -1,8 +0,0 @@
-<?php require_once("tools.php"); ?>
-<?php require_once("header.php"); ?>
-<main class="container">
-
-     <?php print_letter(1); ?>
-</main>
-
-<?php require_once("footer.php"); ?>
